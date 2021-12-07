0d4784f3bf598c5f9cf60f260da1c39baf7fdc8e
Mariana <lugo.mariana.i@gmail.com>
2021-12-07 05:14:19 -0600

    Adding API and yml file docker compose

--- docker/plumber.R
+++ docker/plumber.R
@@ -9,7 +9,8 @@ library(tidyverse)
 library(tidymodels)
 library(rapidoc)
 
-#* @post /prediccion_hd
+#* Endpoint para la predicción de nuevos datos.
+#* @post /prediction
 prediccion_hd <- function(req){
   example <- req$postBody
   parsed_example <- jsonlite::fromJSON(example)
@@ -20,6 +21,7 @@ prediccion_hd <- function(req){
 
 }
 
+#* Endpoint para incorporar nuevos datos a postgres.
 #* @post /new_data
 new_data<-function(req) {
   example <- req$postBody
@@ -40,41 +42,122 @@ new_data<-function(req) {
 
 }
 
+#* Endpoint para reentrenar el modelo.
+#* @post /re_train
+reentrenamiento <- function(req){
 
+  con<-DBI::dbConnect(
+    drv = RPostgres::Postgres(),
+    dbname = 'health',
+    host = "db",
+    user = 'postgres',
+    password = 'postgres',
+    port = 5432
+  )
 
+  data <- tbl(con, "heart") %>% collect()
+  heart = data %>% mutate(heartdisease = as_factor(heartdisease))
+
+  heart_split <- initial_split(heart, prop = 0.80)
+  heart_train <- training(heart_split)
+  heart_test  <-  testing(heart_split)
+
+  n_train<-nrow(heart_train)
+  n_test<-nrow(heart_test)
+  suma<-sum(n_train,n_test)
+
+  heart_recipe <- recipe(heartdisease ~ ., data = heart_train) %>%
+    step_mutate(cholesterol = ifelse(cholesterol== 0 , median (cholesterol), cholesterol) )%>%
+    step_normalize(all_numeric_predictors()) %>%
+    step_dummy(all_nominal_predictors())
+  
+  juice_train <- juice(prep(heart_recipe))
+  bake_test <- bake(prep(heart_recipe), heart_test)
+  
+  # Specify the model
+  logistic_model <- logistic_reg() %>% set_engine("glm")
+  logistic_fit <- fit(logistic_model, heartdisease ~ ., juice_train)
+
+  readr::write_rds(logistic_fit, "logistic_model.Rds")
+  readr::write_rds(heart_recipe, "recipe.Rds")
+  readr::write_rds(heart_test, "heart_test_dataset.Rds")
+
+  print(paste("Reentrenamiento Completado. Entrenamiento:", n_train," Test:", n_test," Total:", suma))
+}
+
+#Endpoint regresa el AUC de la curva ROC del modelo.
+#* @get /metrics
+function(){
+
+  model <- readRDS("logistic_model.Rds")
+  recipe <- readRDS("recipe.Rds")
+  heart_test <- readRDS("heart_test_dataset.Rds")
+ 
+  bake_test <- bake(prep(recipe), heart_test)
+  results_cla_logistico <- predict(model, bake_test, type = 'prob') %>%
+    bind_cols(bake_test)
+  
+  roc_curve_cla_logistico <- roc_curve(
+    results_cla_logistico, 
+    truth = heartdisease, 
+    estimate=.pred_1, event_level = 'second'
+  ) 
+
+  auc_roc<-roc_auc(
+    results_cla_logistico,
+    truth = heartdisease, 
+    .pred_1,event_level = 'second'
+  )
+
+  print(auc_roc)
+}
+
+#* Endpoint de grafica CURVA ROC
 #* @serializer contentType list(type='image/png')
-#* @get /plot
+#* @get /roc_curve_plot
 function(){
   model <- readRDS("logistic_model.Rds")
   recipe <- readRDS("recipe.Rds")
   heart_test <- readRDS("heart_test_dataset.Rds")
+
   bake_test <- bake(prep(recipe), heart_test)
   results_cla_logistico <- predict(model, bake_test, type = 'prob') %>%
     bind_cols(bake_test)
-  roc_curve_cla_logistico <- roc_curve(results_cla_logistico, truth = heartdisease, estimate
-                                       =.pred_1, event_level = 'second') %>%
-    mutate(ID = 'Logit')
+  roc_curve_cla_logistico <- roc_curve(
+    results_cla_logistico, 
+    truth = heartdisease, 
+    estimate=.pred_1, event_level = 'second'
+  ) 
+  
   plot <- autoplot(roc_curve_cla_logistico) + ggtitle("ROC curve")
-  file <- "plot.png"
+  file <- "curva_roc.png"
   ggsave(file, plot)
+  
   readBin(file, "raw", n = file.info(file)$size)
 }
 
+
+#* Endpoint de grafica PRECISION_RECALL
 #* @serializer contentType list(type='image/png')
-#* @get /plot2
+#* @get /pr_curve_plot
 function(){
   model <- readRDS("logistic_model.Rds")
   recipe <- readRDS("recipe.Rds")
   heart_test <- readRDS("heart_test_dataset.Rds")
+
   bake_test <- bake(prep(recipe), heart_test)
   results_cla_logistico <- predict(model, bake_test, type = 'prob') %>%
     bind_cols(bake_test)
-  pr_curve_cla_logistico <- pr_curve(results_cla_logistico, truth = heartdisease, estimate
-                                     =.pred_1, event_level = 'second') %>%
-    mutate(ID = 'Logit')
+  pr_curve_cla_logistico <- pr_curve(
+    results_cla_logistico, 
+    truth = heartdisease, 
+    estimate =.pred_1, event_level = 'second'
+  )
+
   plot2 <- autoplot(pr_curve_cla_logistico) + ggtitle("Precision-recall")
-  file2 <- "plot2.png"
+  file2 <- "pr_plot.png"
   ggsave(file2, plot2)
+  
   readBin(file2, "raw", n = file.info(file2)$size)
 }
 
